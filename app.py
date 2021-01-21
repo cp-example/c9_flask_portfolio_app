09176968f5eff6c5abb543633cdcc5f817d5c894
AsKsKenny <34576273+AsKsKenny@users.noreply.github.com>
2021-01-21 11:50:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,9 +91,15 @@ def python_apps_page():
 def contact():
 	return render_template('contact.html')
 
-@app.route('/password_generator')
+@app.route('/password_generator', methods=['GET','POST'])
 def password_generator():
-	return render_template('password_generator.html')
+	if request.method == 'GET':
+		return render_template('password_generator.html')
+	elif request.method == 'POST':
+		chars = string.digits + string.ascii_letters + string.punctuation
+		print((len)(chars))
+		print(''.join(secrets.choice(chars) for _ in range (40)))
+	
 
 @app.route('/blog', methods=['GET'])
 def blog_page():
